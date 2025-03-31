24863745165143429dae1fff6e84ea384fb2501a
ojaswi <ojaswi.kunwar@gmail.com>
2025-03-31 22:42:27 +0545

    Add input validation to temperature converter

--- temperature_converter.py
+++ temperature_converter.py
@@ -7,14 +7,30 @@ def fahrenheit_to_celsius(fahrenheit):
     return celsius
 
 print("Temperature Converter")
-unit = input("Enter the unit (C for Celsius, F for Fahrenheit):")
-temperature = float(input("Enter the temperature:"))
+while True:
+    unit = input("Enter the unit (C for Celsius, F for Fahrenheit or Q to quit.):").upper()
+    if unit == "Q":
+        print("Goodbye! 👋")
+        break
+    if unit not in ["C", "F"]:
+        print("❌ Invalid unit! Please use C, F, or Q to quit.")
+        continue
+    
 
-if unit == "C":
-    result = celsius_to_fahrenheit(temperature)
-    print(f"{temperature}C is {result}F")
-elif unit == "F":
-    result = fahrenheit_to_celsius(temperature)
-    print(f"{temperature}F is {result}C")
-else:
-    print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+    try:
+        temperature = float(input("Enter the temperature: "))
+    except ValueError:
+        print("❌ Invalid input! Please enter a numeric temperature.")
+        continue
+
+
+#temperature = float(input("Enter the temperature:"))
+
+    if unit== "C":
+        result = celsius_to_fahrenheit(temperature)
+        print(f"{temperature}C is {result}F")
+    elif unit== "F":
+        result = fahrenheit_to_celsius(temperature)
+        print(f"{temperature}F is {result}C")
+    else:
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
